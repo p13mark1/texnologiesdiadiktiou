455a67347f707243d869526dd84028b7903334e3
p13mark1 <p13mark1@ionio.gr>
2023-09-25 21:58:18 +0300

    Update and rename purposeofwebsite to purposeofwebsite.php

---
+++ purposeofwebsite.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Βοήθεια</title>
+    <link rel="stylesheet" href="styles.css">
+    <script src="script.js"></script>
+</head>
+<body>
+    <header>
+        <h1>Βασική Βοήθεια</h1>
+        <button id="toggleTheme">Εναλλαγή Θέματος</button>
+    </header>
+    <section id="help">
+        <h2>Πώς να Χρησιμοποιήσετε τον Ιστότοπο</h2>
+        <p>Στη σελίδα αυτή θα βρείτε βασικές οδηγίες για το πώς να αναρτήσετε ερωτήσεις, να αναζητήσετε απαντήσεις και να συμμετέχετε στην κοινότητα τεχνικών ερωτήσεων.</p>
+    </section>
+</body>
+</html>
